8c756cc8f40547f573c0512b066995098c73221b
jyoung0613 <jwyoung0613@gmail.com>
2021-11-22 08:03:41 -0700

    found issue with images added dot public to address

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -220,14 +220,14 @@ $(document).ready(function() {
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: "./public/assets/img/food-table.jpg",
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: "./public/assets/img/grill.jpg",
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
