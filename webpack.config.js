6fd3b0acfa3c45a71044ec2f5ae674ad4366ad4d
jyoung0613 <jwyoung0613@gmail.com>
2021-11-24 19:04:00 -0700

    fixed bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-// const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
 
 const config = {
@@ -47,20 +45,6 @@ const config = {
     new BundleAnalyzerPlugin({
       analyzerMode: 'static'
     })
-    // new WebpackPwaManifest({
-    //   name: "Food Fest",
-    //   short_name: "Foodies",
-    //   description: "An app that allows you to view upcoming food events.",
-    //   background_color: "#01579b",
-    //   theme_color: "#ffffff",
-    //   fingerprints: false,
-    //   inject: false,
-    //   icons: [{
-    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
-    //     sizes: [96, 128, 192, 256, 384, 512],
-    //     destination: path.join("assets", "icons")
-    //   }]
-    // })
   ],
   mode: 'development'
 };
